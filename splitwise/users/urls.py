68aa6924aca9dc2c31fd6b7ce334cf3640bb48fa
kushagra <kushagra.agrawal@creditas.in>
2024-09-02 22:49:36 +0530

    setup done

--- splitwise/users/urls.py
+++ splitwise/users/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-import views
+from users import  views
 
 urlpatterns = [
     path('signup/', views.handle_signup),
--- splitwise/users/views.py
+++ splitwise/users/views.py
@@ -1,15 +1,12 @@
-from django.contrib.messages.api import success
 from django.http.response import HttpResponse
-from newrelic.packages.requests.models import json_dumps
-from rest_framework.response import Response
-from rest_framework import status
+import json
 
 
 def handle_signup(request):
-    return HttpResponse(json_dumps({"success":True}),content_type="application/json")
+    return HttpResponse(json.dumps({"success":True}),content_type="application/json")
     # return Response({'name':'kushagra'},status=status.HTTP_200_OK)
 
 
 def handle_login(request):
-    return HttpResponse(json_dumps({"success":True}),content_type="application/json")
+    return HttpResponse(json.dumps({"success":True}),content_type="application/json")
     # return Response({'name':'kushagra'},status=status.HTTP_200_OK)
